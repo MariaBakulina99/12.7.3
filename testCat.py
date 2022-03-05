baad709917c1866260f79aa6592459213a3adcb9
MariaBakulina99 <95924857+MariaBakulina99@users.noreply.github.com>
2022-03-05 08:19:24 -0500

    Задание 16.7.1

---
+++ testCat.py
@@ -0,0 +1,12 @@
+from cat import Cat
+
+Cat_1 = Cat('Baron', 'male', 2)
+Cat_2 = Cat('Sam', 'male', 2)
+
+print("Cat_1.name=", Cat_1.name)
+print("Cat_1.gender=", Cat_1.gender)
+print("Cat_1.age=", Cat_1.getAge())
+print('\n')
+print("Cat_2.name=", Cat_2.getName())
+print("Cat_2.gender=", Cat_2.getGender())
+print("Cat_2.age=", Cat_2.age)
